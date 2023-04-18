ed37a7df95717a89d02baa4d9d9e698f265c1197
ErwinFeng <xfsy_2015@163.com>
2023-04-18 15:06:22 +0800

    eureka server and client

--- demo-spring-cloud-eureka/eureka-client/src/main/java/com/fengwenyi/demo/springcloud/eureka/client/EurekaClientApplication.java
+++ demo-spring-cloud-eureka/eureka-client/src/main/java/com/fengwenyi/demo/springcloud/eureka/client/EurekaClientApplication.java
@@ -1,5 +1,19 @@
-package com.fengwenyi.demo.springcloud.eureka.client;/**
+package com.fengwenyi.demo.springcloud.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
  * @author <a href="https://fengwenyi.com">Erwin Feng</a>
  * @since 2023-04-18
- */public class EurekaClientApplication {
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
 }
--- demo-spring-cloud-eureka/eureka-server/src/main/java/com/fengwenyi/demo/springcloud/eureka/server/EurekaServerApplication.java
+++ demo-spring-cloud-eureka/eureka-server/src/main/java/com/fengwenyi/demo/springcloud/eureka/server/EurekaServerApplication.java
@@ -1,5 +1,19 @@
-package com.fengwenyi.demo.springcloud.eureka.server;/**
+package com.fengwenyi.demo.springcloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
  * @author <a href="https://fengwenyi.com">Erwin Feng</a>
  * @since 2023-04-18
- */public class EurekaServerApplication {
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
 }
